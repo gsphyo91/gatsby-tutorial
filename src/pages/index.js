2a6fed841a6559fd07d84d38f93e0eba111702fc
Gyungsoo Phyo <gsphyo91@gmail.com>
2022-02-04 16:15:46 +0900

    feat: add index file for export

--- src/pages/index.js
+++ src/pages/index.js
@@ -1,6 +1,6 @@
 import { StaticImage } from "gatsby-plugin-image";
 import React from "react";
-import Layout from "./components/Layout/Layout";
+import { Layout } from "./components";
 
 // markup
 const IndexPage = () => {
